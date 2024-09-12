b2e68691b2c858c5bc902e3f1e8348a41b96295a
Ornitorink0 <matteogurri1@gmail.com>
2024-09-12 16:18:10 +0200

    Init

---
+++ src/routes/HomePage.tsx
@@ -0,0 +1,18 @@
+import { Box } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <Box
+      width={"100%"}
+      height={"100vh"}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      {/* TODO */}
+      <h1>TODO</h1>
+    </Box>
+  );
+};
+
+export default HomePage;
