4996a1bfcf561abcc8a9ad173fe7d01a9a148688
Ornitorink0 <matteogurri1@gmail.com>
2024-10-06 20:48:53 +0200

    Aggiustato

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,18 +1,31 @@
 import React, { PureComponent } from "react";
 import NavigationBar from "../components/NavigationBar";
+import MetaTags from "../Meta";
 
 export default class NoPage extends PureComponent {
   render() {
     return (
       <>
-        <MetaTags title="Pagina non trovata" />
+        <MetaTags
+          title="Pagina non trovata"
+          description="Pagina non trovata"
+          name="Pagina non trovata"
+          locale="it_IT"
+          robots="index,follow"
+          organization="SobreWeb"
+          logo="/logo-compat-blue.png"
+          siteUrl="https://sobreweb.it"
+          image="/logo-compat-blue.png"
+        />
         <NavigationBar />
         <div className="flex min-h-screen flex-col items-center justify-center bg-primary-900 p-10 text-center text-3xl font-bold text-white">
           <h1 className="mb-4 text-6xl font-extrabold">Pagina non trovata</h1>
           <a href="/home" className="text-blue-500 hover:underline">
             Torna alla home
           </a>
-          <p className="text-lg text-slate-600">Errore 404: pagina non trovata.</p>
+          <p className="text-lg text-slate-600">
+            Errore 404: pagina non trovata.
+          </p>
         </div>
       </>
     );
