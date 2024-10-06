14162c3a8126f5cdb1de7f17d5607714613d7fa9
Ornitorink0 <matteogurri1@gmail.com>
2024-10-06 20:36:27 +0200

    Pagina non trovata modificata

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,7 +1,19 @@
 import React, { PureComponent } from "react";
+import NavigationBar from "../components/NavigationBar";
 
 export default class NoPage extends PureComponent {
   render() {
-    return <div>NoPage</div>;
+    return (
+      <>
+        <NavigationBar />
+        <div className="flex min-h-screen flex-col items-center justify-center bg-primary-900 p-10 text-center text-3xl font-bold text-white">
+          <h1 className="mb-4 text-6xl font-extrabold">Pagina non trovata</h1>
+          <a href="/home" className="text-blue-500 hover:underline">
+            Torna alla home
+          </a>
+          <p className="text-lg text-slate-600">Errore 404: pagina non trovata.</p>
+        </div>
+      </>
+    );
   }
 }
